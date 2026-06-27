63901d9e52d386282e16425bef30b0308f3a8158
abrar <mohammedabrar934@gmail.com>
2026-06-27 12:20:19 +0530

    minor changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function AboutPage() {
       <MissionVisionSection />
       <QualificationsTimeline />
       <ExpertiseSection />
-      <CareerTimelineSection />
+      
     </PageTransition>
   );
 }
